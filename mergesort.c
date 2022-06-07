d6f117c0c176ce5806dc6a497cd53de4c0823538
webgtx <dxv1d@tuta.io>
2022-06-07 17:39:45 +0200

    Update: fork to another branch for onefile programm

--- mergesort.c
+++ mergesort.c
@@ -1,6 +1,9 @@
 // webgtx @ 2020
 // dxv1d@tuta.io
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include "./lib/dxtlib.c" // My own lib 
+
 // Merges two subarrays of arr[].
 // First subarray is arr[l..m]
 // Second subarray is arr[m+1..r]
@@ -75,4 +78,26 @@ void printArray(int A[], int size)
 	for (i = 0; i < size; i++)
 		printf("%d ", A[i]);
 	printf("\n");
+}
+
+int main(int argc, char **argv)
+{
+  int acntr = argc;
+  int arr[10];
+  puts("|  IDX  | INT | LEN");
+  for (unsigned idx = 0; idx < acntr; idx++) {
+    arr[idx] = atoi(*argv++);
+    printf("idx: %2i %5i %5i\n", idx, arr[idx], acntr);
+  }
+	// int arr[] = { 12, 11, 13, 5, 6, 7 };
+	int arr_size = sizeof(arr) / sizeof(arr[0]);
+
+	printf("Given array is \n");
+	printArray(arr, arr_size);
+
+	mergeSort(arr, 0, arr_size - 1);
+
+	printf("\nSorted array is \n");
+	printArray(arr, arr_size);
+	return 0;
 }
